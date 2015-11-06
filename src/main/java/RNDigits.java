fb3285c5e0cbd737728bf6b1a2cfa87670a955e6
Isaac Madwed <i.madwed@gmail.com>
2015-11-05 23:01:57 -0500

    fixes up rndigits types, cleans build.gradle

--- src/main/java/RNDigits.java
+++ src/main/java/RNDigits.java
@@ -1,18 +1,25 @@
 package co.fixt.react.digits; 
 
+import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
 
 import com.digits.sdk.android.Digits;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterCore;
 import com.digits.sdk.android.AuthCallback;
 import com.digits.sdk.android.DigitsSession;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsOAuthSigning;
+
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterAuthToken;
 import io.fabric.sdk.android.Fabric;
+
+import java.util.Map;
 
 public final class RNDigits extends ReactContextBaseJavaModule {
   // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
@@ -22,8 +29,8 @@ public final class RNDigits extends ReactContextBaseJavaModule {
 
   public RNDigits(ReactApplicationContext reactContext) {
     // This probably needs to go in an init method?
-    TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-    Fabric.with(this, new TwitterCore(authConfig), new Digits());
+    //TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+    //Fabric.with(this, new TwitterCore(authConfig), new Digits());
 
     super(reactContext);
   }
@@ -39,23 +46,29 @@ public final class RNDigits extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void view(Callback cb) {
+  public void view(final Callback cb) {
     callback = new AuthCallback() {
       @Override
       public void success(DigitsSession session, String phoneNumber){
         WritableMap auth = Arguments.createMap();
 
-        TwitterAuthConfig config = Digits.getAuthConfig();
+        Digits digits = Digits.getInstance();
+
+        TwitterAuthConfig config = digits.getAuthConfig();
         auth.putString("consumerKey", config.getConsumerKey());
         auth.putString("consumerSecret", config.getConsumerSecret());
 
-        TwitterAuthToken token = session.getAuthToken();
+        TwitterAuthToken token = (TwitterAuthToken)session.getAuthToken();
+        //DigitsOAuthSigning oAuthSigning = new DigitsOAuthSigning(config, token);
+        //Map<String, String> oAuthHeaders = oAuthSigning.getOAuthEchoHeadersForVerifyCredentials();
+
         auth.putString("authToken", token.token);
         auth.putString("authTokenSecret", token.secret);
 
-        auth.putString("userId", session.getId());
+        Long id = session.getId();
+        auth.putString("userId", String.valueOf(id));
         auth.putString("phoneNumber", phoneNumber);
-        // consumerKey, consumerSecret 
+
         cb.invoke(null, auth);
       }
 
